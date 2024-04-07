8245718510b8d522c7675e4cea9545b50fc43adb
Robert Castro <oneroberth@gmail.com>
2024-04-07 13:22:40 -0500

    Up

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,10 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return '¡Hola, mundo! Esta es la aplicación Flask en desarrollo.'
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+from app import app
