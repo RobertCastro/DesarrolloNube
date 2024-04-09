7e639480c0aac5a0ca5d1272fc805f1f2236ba55
root <root@FSOFTWARE04.rtvc.gov.co>
2024-04-09 10:05:09 -0500

    up

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,1 +1,10 @@
-from app import app
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    return '¡Flask app running in a Docker container! 🐳🐍🚀'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=True)
