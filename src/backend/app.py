ca096caf6d983647a361da680f062a21220f0b98
root <root@FSOFTWARE04.rtvc.gov.co>
2024-04-10 17:47:47 -0500

    Up

--- src/backend/app.py
+++ src/backend/app.py
@@ -14,7 +14,7 @@ jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://usuario:contraseña@localhost/nombre_base_datos'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:admin@localhost/idlr_db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['JWT_SECRET_KEY'] = 'super-secreto'
 
--- src/backend/models/models.py
+++ src/backend/models/models.py
@@ -1,23 +1,33 @@
-from ..app import db  # Importar la instancia de SQLAlchemy creada en __init__.py
-from werkzeug.security import generate_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
-class Usuario(db.Model):
+# Inicialización del objeto SQLAlchemy
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    password_hash = db.Column(db.String(100), nullable=False)
-    videos = db.relationship('Video', backref='usuario', lazy=True)
+    username = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(255), nullable=False)  # Asumo que quisiste referirte a 'password' en lugar de 'password1'
+    email = db.Column(db.String(50), nullable=False, unique=True)
 
-    @property
-    def password(self):
-        raise AttributeError('La contraseña no es un atributo legible.')
+    # Relación uno a muchos: Un usuario puede tener muchas tareas
+    tasks = db.relationship('Task', backref='user', lazy=True)
 
-    @password.setter
-    def password(self, password):
-        self.password_hash = generate_password_hash(password)
+    def __repr__(self):
+        return f'<User {self.username}>'
 
-class Video(db.Model):
+class Task(db.Model):
+    __tablename__ = 'tasks'
+
     id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(100), nullable=False)
-    estado = db.Column(db.String(20), default='uploaded')  # 'uploaded', 'processed'
-    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    status = db.Column(db.String(255), nullable=False)
+    file_path = db.Column(db.String(255), nullable=False)
+    
+    # Clave foránea para asociar una tarea con un usuario
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Task {self.id}, Status: {self.status}>'
